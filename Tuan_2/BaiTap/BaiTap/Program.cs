fe4ba6d3024b5fef1157fafe75817b4b5504c02e
hoantran1107 <hoantran1107@gmail.com>
2022-12-04 11:44:43 +0700

    cap nhat

---
+++ Tuan_2/BaiTap/BaiTap/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BaiTap
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //lay ra current env 
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true)
+                .Build();
+            //goi ConfigureLogging tao o duoi
+            ConfigureLogging(environment, configuration);
+            //create host
+            try
+            {
+                Log.Information("Starting host.");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host failure.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static void ConfigureLogging(string environment, IConfigurationRoot configuration)
+        {
+            //tao ra LoggerConfiguration đe them thong tin ve evn name, machine name, excution context
+            //Sinksearch co tham so vào url của elasticSreach , 2 la indexFormat
+            // indexFormat là gồm System.Reflection.Assembly name,ten moi truong,ngay
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .Enrich.WithEnvironmentName()
+                .Enrich.WithMachineName()
+                .WriteTo.Console()
+                .WriteTo.Debug()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticSearch:Url"]))
+                {
+                    AutoRegisterTemplate = true,
+                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                    IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name!.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+                })
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Tuan_2/Readme.md
@@ -0,0 +1,8 @@
+Công việc công ty giao
+ngày 30/11:
+- Tìm hiểu về Filter trong .Net Core
+- Giải bài tập C# cơ bản
+ngày 2/12
+- Tìm hiểu về appseting.json, kibana, elasticsreach
+- serilog, các mức độ log trong asp.net
+- làm bài tập về các kiến thức trên
